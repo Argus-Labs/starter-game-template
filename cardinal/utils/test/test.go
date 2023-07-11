41195844ffa0c63c2545beabe578eb998b0f23d9
smsunarto <github@smsunarto.com>
2023-07-11 16:10:18 -0700

    refactor: move test utils to utils folder

--- cardinal/utils/test/test.go
+++ cardinal/utils/test/test.go
@@ -1,6 +1,11 @@
-package test
+package utils
 
 import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"reflect"
+
 	"github.com/argus-labs/starter-game-template/component"
 	tx "github.com/argus-labs/starter-game-template/msg/tx"
 	"github.com/argus-labs/starter-game-template/system"
@@ -31,4 +36,26 @@ func CreatePlayer(world *ecs.World, tag string) (storage.EntityID, component.Pla
 	}
 	component.Player.Set(world, entityId, playerComp)
 	return entityId, playerComp
+}
+
+func DecodeMsg[T any](r *http.Request, msg *T) error {
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	// Try to decode the request body into struct T.
+	// Errors if msg has unknown fields.
+	err := decoder.Decode(&msg)
+	if err != nil {
+		return err
+	}
+
+	// Check that all fields are present
+	fields := reflect.ValueOf(msg).Elem()
+	for i := 0; i < fields.NumField(); i++ {
+		if fields.Field(i).IsZero() {
+			return errors.New("some msg field(s) are missing")
+		}
+	}
+
+	return nil
 }
