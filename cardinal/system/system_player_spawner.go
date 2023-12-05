e9bbac0cb49d53c3ba7d5b65bf32ee1fde0688bc
smsunarto <github@smsunarto.com>
2023-12-05 11:59:45 -0800

    minor refactor

--- cardinal/system/system_player_spawner.go
+++ cardinal/system/system_player_spawner.go
@@ -12,19 +12,12 @@ import (
 // This provides an example of a system that creates a new entity.
 func PlayerSpawnerSystem(world cardinal.WorldContext) error {
 	msg.CreatePlayer.Each(world, func(create cardinal.TxData[msg.CreatePlayerMsg]) (msg.CreatePlayerResult, error) {
-		id, err := cardinal.Create(world, comp.Player{}, comp.Health{})
+		id, err := cardinal.Create(world,
+			comp.Player{Nickname: create.Msg().Nickname},
+			comp.Health{HP: 100},
+		)
 		if err != nil {
 			return msg.CreatePlayerResult{}, fmt.Errorf("error creating player: %w", err)
-		}
-
-		err = cardinal.SetComponent[comp.Player](world, id, &comp.Player{Nickname: create.Msg().Nickname})
-		if err != nil {
-			return msg.CreatePlayerResult{}, fmt.Errorf("error setting player nickname: %w", err)
-		}
-
-		err = cardinal.SetComponent[comp.Health](world, id, &comp.Health{HP: 100})
-		if err != nil {
-			return msg.CreatePlayerResult{}, fmt.Errorf("error setting player health: %w", err)
 		}
 
 		world.EmitEvent(fmt.Sprintf("new player %d created", id))
